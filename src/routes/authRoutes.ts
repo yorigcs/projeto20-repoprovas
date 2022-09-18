a8904b74bf5c169b03e48b55db05c6f5e6f6b1b7
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 15:40:37 -0300

    feat: ensure correct body is provided

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-
+import { signUpSchema } from '../schemas/authSchemas'
+import { validateBody } from '../middlewares/validateBody'
 export default (router: Router): void => {
-  router.post('/sign-up', (req, res) => res.send('ok'))
+  router.post('/sign-up', validateBody(signUpSchema), (req, res) => res.send('ok'))
 }
