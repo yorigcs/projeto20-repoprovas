cfead5f7c29a496de3769537e1e47ab037f63a3a
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 17:26:21 -0300

    feat: add env vars

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const env = {
-  port: process.env.PORT || 5000
+  port: process.env.PORT || 5000,
+  jwt_secret: process.env.JWT_SECRET || 'secret',
+  jwt_exp: process.env.JWT_EXP || '1d'
 }
