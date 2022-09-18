364fd6801b4d0afa9a6f85138a254531c3b8fd91
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 19:51:12 -0300

    feat: getTestByTerms repository

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -5,3 +5,35 @@ export const createTest = async (testData: TestWithoutId): Promise<Test> => {
   const result = await prismaClient.test.create({ data: testData })
   return result
 }
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const getTestByTerms = async () => {
+  const tests = await prismaClient.term.findMany({
+    select: {
+      id: true,
+      number: true,
+      discipline: {
+        select: {
+          id: true,
+          name: true,
+          TeacherDiscipline: {
+            select: {
+              teacher: true,
+              Test: {
+                select: {
+                  id: true,
+                  name: true,
+                  pdf_url: true,
+                  category_id: true
+                }
+              }
+
+            }
+          }
+        }
+      }
+
+    }
+  })
+  return tests
+}
