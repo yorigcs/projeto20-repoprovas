e6570c0e715c50f504ddaff43a8c738f3f046b43
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 18:40:56 -0300

    feat: create route tests

--- src/routes/testsRoutes.ts
+++ src/routes/testsRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { testSchema } from '../schemas/testsSchemas'
 import { validateBody } from '../middlewares/validateBody'
 import { validateToken } from '../middlewares/validateToken'
+import { testsController } from '../controllers/testsController'
 export default (router: Router): void => {
-  router.post('/term', validateBody(testSchema), validateToken())
+  router.post('/term', validateBody(testSchema), validateToken(), testsController)
 }
