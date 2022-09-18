957dfea974c8b37e628f9720f1ce2ffdfbe2c1db
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 17:10:53 -0300

    feat: create sign-in route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
-import { signUpSchema } from '../schemas/authSchemas'
+import { signInSchema, signUpSchema } from '../schemas/authSchemas'
 import { validateBody } from '../middlewares/validateBody'
 import { signUpController } from '../controllers/signUpController'
+import { signInController } from '../controllers/signInController'
 export default (router: Router): void => {
   router.post('/sign-up', validateBody(signUpSchema), signUpController)
+  router.post('/sign-in', validateBody(signInSchema), signInController)
 }
