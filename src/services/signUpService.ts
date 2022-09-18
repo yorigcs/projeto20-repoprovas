bcdb06aee21e276194465a0c14b457d731fbb81c
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 16:35:05 -0300

    feat: ensure password is encrypted

--- src/services/signUpService.ts
+++ src/services/signUpService.ts
@@ -1,9 +1,14 @@
 import { ok, serverError } from '../helpers/service'
 import { UserWithoutId } from '../models/user'
 import { ServiceResponse } from '../protocols/serviceResponse'
+import { encrypt } from '../utils/bcryptAdapter'
 
 export const signUpService = async (userData: UserWithoutId): Promise<ServiceResponse> => {
   try {
+    const salt = 12
+    const hashedPassword = await encrypt(userData.password, salt)
+    Object.assign(userData, { password: hashedPassword })
+
     return ok(userData)
   } catch (err) {
     return serverError()
