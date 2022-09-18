fe3716b4c9e375fe3e6a9489073ab71e4672b2ba
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 16:58:51 -0300

    feat: ensure user is registered if is valid user

--- src/services/signUpService.ts
+++ src/services/signUpService.ts
@@ -1,16 +1,25 @@
-import { ok, serverError } from '../helpers/service'
-import { UserWithoutId } from '../models/user'
+import { conflict, created, serverError } from '../helpers/service'
+import { UserData } from '../models/user'
 import { ServiceResponse } from '../protocols/serviceResponse'
+import { createUser, findUserByEmail } from '../repositories/userRepository'
 import { encrypt } from '../utils/bcryptAdapter'
 
-export const signUpService = async (userData: UserWithoutId): Promise<ServiceResponse> => {
+export const signUpService = async (userData: UserData): Promise<ServiceResponse> => {
   try {
     const salt = 12
     const hashedPassword = await encrypt(userData.password, salt)
     Object.assign(userData, { password: hashedPassword })
 
-    return ok(userData)
+    const user = await findUserByEmail(userData.email)
+    if (user) {
+      return conflict(new Error('This user is already registered'))
+    }
+    delete userData.confirmPassword
+
+    const regiterUser = await createUser(userData)
+    return created(regiterUser)
   } catch (err) {
+    console.log(err)
     return serverError()
   }
 }
