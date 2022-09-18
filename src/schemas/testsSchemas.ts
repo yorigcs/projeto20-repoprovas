6d043c5ff34feafa9d3f5850541df1d7cb9f6c31
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 18:39:24 -0300

    refactor: change key names

--- src/schemas/testsSchemas.ts
+++ src/schemas/testsSchemas.ts
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 export const testSchema = Joi.object({
   name: Joi.string().required(),
-  pdfUrl: Joi.string().uri().required(),
-  categoryId: Joi.number().min(1).required(),
-  teacherDisciplineId: Joi.number().min(1).required()
+  pdf_url: Joi.string().uri().required(),
+  category_id: Joi.number().min(1).required(),
+  teacher_discipline_id: Joi.number().min(1).required()
 })
