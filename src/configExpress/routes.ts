5f3cd50dac20aa0bb7c2d864df845707b88dc375
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-19 13:46:43 -0300

    refactor: change fast-glob to readdirSync

--- src/configExpress/routes.ts
+++ src/configExpress/routes.ts
@@ -1,10 +1,10 @@
-import fg from 'fast-glob'
 import { Express, Router } from 'express'
-
+import { readdirSync } from 'fs'
+import path from 'path'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
 
-  fg.sync('**/src/routes/**Routes.*.{js,ts}')
-    .map(async file => (await import(`../../../${file}`)).default(router))
+  readdirSync(path.join(__dirname, '/../routes'))
+    .map(async file => (await import(`../routes/${file}`)).default(router))
 }
