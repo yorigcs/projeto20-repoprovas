417417832cb2144d25a2a1811b742adc8bee6a1d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-19 02:31:21 -0300

    refactor: change to single file authRoutes

--- tests/authRoutes.test.ts
+++ tests/authRoutes.test.ts
@@ -1,7 +1,16 @@
 import supertest from 'supertest'
+import { prismaClient } from '../prisma/prismaClient'
 import app from '../src/configExpress/app'
-import { createUser } from '../src/repositories/userRepository'
-import { signUp } from './factories/signUp'
+import { signIn, signInCreateAccount } from './factories/signIn'
+import { signUp, signUpRegisterFakerUser } from './factories/signUp'
+
+beforeEach(async () => {
+  await prismaClient.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY CASCADE`
+})
+
+afterAll(async () => {
+  await prismaClient.$disconnect()
+})
 
 describe('POST -> SignUp route', () => {
   test('should returns status 422 if email is not provided', async () => {
@@ -45,7 +54,7 @@ describe('POST -> SignUp route', () => {
   test('should returns status 409 if has conflict', async () => {
     const body = signUp()
     const { confirmPassword, ...bodyWithoutConfirmPassword } = body
-    await createUser(bodyWithoutConfirmPassword)
+    await signUpRegisterFakerUser(bodyWithoutConfirmPassword)
     const sut = await supertest(app).post('/api/sign-up').send(body)
     expect(sut.status).toBe(409)
   })
@@ -54,5 +63,54 @@ describe('POST -> SignUp route', () => {
     const body = signUp()
     const sut = await supertest(app).post('/api/sign-up').send(body)
     expect(sut.status).toBe(201)
+  })
+})
+
+describe('POST -> SignIn route', () => {
+  test('should returns status 422 if email is not provided', async () => {
+    const body = signIn()
+    const { email, ...bodyWithoutEmail } = body
+
+    const sut = await supertest(app).post('/api/sign-in').send(bodyWithoutEmail)
+    expect(sut.status).toBe(422)
+  })
+
+  test('should returns status 422 if an invalid email is provided', async () => {
+    const body = signIn()
+    const bodyWithWrongEmail = Object.assign(body, { email: 'invalidEmail' })
+
+    const sut = await supertest(app).post('/api/sign-in').send(bodyWithWrongEmail)
+    expect(sut.status).toBe(422)
+  })
+
+  test('should returns status 422 if password is not provided', async () => {
+    const body = signIn()
+    const { password, ...bodyWithoutPassword } = body
+
+    const sut = await supertest(app).post('/api/sign-in').send(bodyWithoutPassword)
+    expect(sut.status).toBe(422)
+  })
+
+  test('should returns status 404 if password is wrong', async () => {
+    const body = signIn()
+    await signInCreateAccount(body)
+
+    const sut = await supertest(app).post('/api/sign-in')
+      .send(Object.assign({}, body, { password: 'any_password' }))
+    expect(sut.status).toBe(404)
+  })
+
+  test('should returns status 404 if there is no account with this email', async () => {
+    const body = signIn()
+    const sut = await supertest(app).post('/api/sign-in').send(body)
+    expect(sut.status).toBe(404)
+  })
+
+  test('should returns status 200 if credentials are correct', async () => {
+    const body = signIn()
+    await signInCreateAccount(body)
+
+    const sut = await supertest(app).post('/api/sign-in').send(body)
+    expect(sut.status).toBe(200)
   })
 })
