fa763f69e2f836353a7eb34e5395d22d9b6fb1e5
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-19 03:27:02 -0300

    fix: ensure build is maped

--- src/configExpress/routes.ts
+++ src/configExpress/routes.ts
@@ -5,6 +5,6 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
 
-  fg.sync('**/src/routes/**Routes.ts')
-    .map(async file => (await import(`../../${file}`)).default(router))
+  fg.sync('**/src/routes/**Routes.*.{js,ts}')
+    .map(async file => (await import(`../../../${file}`)).default(router))
 }
