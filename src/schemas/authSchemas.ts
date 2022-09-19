4289f964c7828e889cd2db0e4e2feabe8f081bab
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-18 21:36:26 -0300

    fix: ensure confirmPassword is provided

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -3,7 +3,7 @@ import Joi from 'joi'
 export const signUpSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-  confirmPassword: Joi.ref('password')
+  confirmPassword: Joi.string().required().valid(Joi.ref('password'))
 })
 
 export const signInSchema = Joi.object({
